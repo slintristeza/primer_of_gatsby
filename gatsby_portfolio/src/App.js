cce04ac7e08a06facd444cd7a86ca3515a8b1a0b
slintristeza <slintristeza@gmail.com>
2019-08-08 22:53:58 +0900

    added Helmet

--- gatsby_portfolio/src/App.js
+++ gatsby_portfolio/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import './App.css';
+import { Helmet } from 'react-helmet';
 
+import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -10,6 +11,11 @@ import Footer from './components/Footer';
 const App = () => {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>slintristeza Portfolio</title>
+        <meta name="description" content="primer of Gatyby.js" />
+      </Helmet>
       <Header />
       <Hero />
       <About />
