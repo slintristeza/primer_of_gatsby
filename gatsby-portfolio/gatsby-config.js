50f3bdd33efaeabffa8fb74bb4cc7057be33a6e7
slintristeza <slintristeza@gmail.com>
2019-08-10 16:26:48 +0900

    mod:siteMetadata

--- gatsby-portfolio/gatsby-config.js
+++ gatsby-portfolio/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `First GraphQL`,
+    description: `My firsl writting GraphQL`,
+    author: `@slintristeza`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
