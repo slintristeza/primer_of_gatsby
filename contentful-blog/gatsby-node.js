86da3216ebea59d0ea239892cb5f44941ec6ef40
slintristeza <slintristeza@gmail.com>
2019-08-11 06:30:33 +0900

    feat:posts template

--- contentful-blog/gatsby-node.js
+++ contentful-blog/gatsby-node.js
@@ -1,7 +1,30 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  const postTemplate = path.resolve(`./src/templates/post.js`)
+  return new Promise((resolve, reject) => {
+    graphql(`
+    {
+      allContentfulPost {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+    `).then(result => {
+      result.data.allContentfulPost.edges.forEach(edge => {
+        createPage({
+          path: `/posts/${edge.node.slug}`,
+          component: postTemplate,
+          context: {
+            slug: edge.node.slug,
+          }
+        })
+      })
+      resolve()
+    })
+  })
+}
---
+++ contentful-blog/src/templates/post.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const post = () => {
+  return (
+    <p>This is post template</p>
+  )
+}
+
+export default post
