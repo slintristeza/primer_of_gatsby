cba46acf193a18e593abd23ef69103d1633b4445
slintristeza <slintristeza@gmail.com>
2019-08-10 17:35:37 +0900

    add:styled-components

--- gatsby-portfolio/gatsby-config.js
+++ gatsby-portfolio/gatsby-config.js
@@ -22,6 +22,7 @@ module.exports = {
     },
     `gatsby-transformer-yaml`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
--- gatsby-portfolio/src/pages/about.js
+++ gatsby-portfolio/src/pages/about.js
@@ -1,12 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+const Title = styled.h1`
+  font-size: 24px;
+  color: red;
+`;
 
 const AboutPgae = () => (
   <Layout>
     <SEO title="about" />
-    <h1>I"m slintristeza</h1>
+    <Title>I"m slintristeza</Title>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
