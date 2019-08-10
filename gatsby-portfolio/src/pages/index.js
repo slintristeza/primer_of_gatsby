45e2b9601f9c836715e07b47ea85bb8f4c53f14a
slintristeza <slintristeza@gmail.com>
2019-08-10 17:03:09 +0900

    mod:view GraphQL data

--- gatsby-portfolio/src/pages/index.js
+++ gatsby-portfolio/src/pages/index.js
@@ -1,15 +1,38 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hello Great Gatsby</h1>
+    {data.allStudyYaml.edges.map(edge => {
+      const study = edge.node
+      return (
+        <div>
+          {study.title} - {study.category} - {study.year}/{study.month}
+        </div>
+      )
+    })}
     <Link to="/about/">Go to about page</Link>
   </Layout>
 )
+
+export const query = graphql`
+  {
+    allStudyYaml {
+      edges {
+        node {
+          title
+          category
+          year
+          month
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
