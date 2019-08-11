965bea775590c1509346506b881abad5734c9616
slintristeza <slintristeza@gmail.com>
2019-08-11 21:29:45 +0900

    feat:view post.js

--- contentful-blog/src/templates/post.js
+++ contentful-blog/src/templates/post.js
@@ -1,9 +1,51 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
 
-const post = () => {
+const post = props => {
+  const post = props.data.contentfulPost
+  const author = post.author
+  const avatar = author.avatar
+  const contentHtml = post.content.childMarkdownRemark.html
   return (
-    <p>This is post template</p>
+    <Layout>
+      <h1>{post.title}</h1>
+      {avatar && (
+        <img
+          width={40} height={40 / avatar.fixed.width * avatar.fixed.height}
+          src={avatar.fixed.src} alt={author.name}
+        />
+      )}
+      <small>
+        {author.name} | {post.publishedAt}
+      </small>
+      <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+    </Layout>
   )
 }
 
 export default post
+
+export const query = graphql`
+  query($slug: String!) {
+    contentfulPost(slug: { eq: $slug }) {
+      title
+      publishedAt(formatString: "YYYY?MM?DD")
+      content {
+        childMarkdownRemark {
+          html
+        }
+      }
+      author {
+        name
+        avatar {
+          fixed {
+            width
+            height
+            src
+          }
+        }
+      }
+    }
+  }
+`
